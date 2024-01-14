50e661804fd09243988dfeca704ec502d4b54f24
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-01-14 17:45:05 +0900

    ui 개선

--- test_weather/CurrentTempView.swift
+++ test_weather/CurrentTempView.swift
@@ -25,22 +25,33 @@ struct CurrentTempView: View {
         let weather = weatherKitManager.getWeatherIconForCondition(condition: weatherKitManager.getWeathers(day: today)?.condition)
         
         Divider().frame(height: 2).background(Color("text"))
-        VStack(spacing: -20){
+        VStack(){
             Image("\(weather)")
                 .renderingMode(.template)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .foregroundColor(Color("text"))
                 .frame(width: 60, height: 60)
-            Text("\(tempDifference > 0 ? "+\(tempDifference)" : "\(tempDifference)")")
-                .font(.system(size: 130))
-                .fontWeight(.semibold)
-                .foregroundColor(Color("text"))
-                .frame(maxWidth: .infinity, alignment: .center)
-            Text("\(todayTemp)°C")
-                .foregroundColor(Color("text"))
-                .frame(maxWidth: .infinity, alignment: .center)
-                .font(.system(size: 25))
+            VStack(spacing: -20){
+                Text(NSLocalizedString("TempDifference", comment: "어제와 오늘의 온도차"))
+                    .font(.system(size: 20))
+                    .foregroundColor(Color("text"))
+                Text("\(tempDifference > 0 ? "+\(tempDifference)" : "\(tempDifference)")")
+                    .font(.system(size: 130))
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color("text"))
+
+            }.frame(maxWidth: .infinity, alignment: .center)
+            
+            HStack{
+                Text(NSLocalizedString("NowTemp", comment: "현재 온도"))
+                    .foregroundColor(Color("text"))
+                    .font(.system(size: 20))
+                Text("\(todayTemp)°C")
+                    .foregroundColor(Color("text"))
+                    .font(.system(size: 30))
+            }.frame(maxWidth: .infinity, alignment: .center)
+            
             
         }
         .frame(maxHeight: .infinity)
